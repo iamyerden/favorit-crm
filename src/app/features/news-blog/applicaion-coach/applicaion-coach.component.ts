348c38d094ef585882b14a195de3d38da15dedb0
Miras <Miras@gmail.com>
2022-02-20 17:56:42 +0500

    Front CRM

---
+++ src/app/features/news-blog/applicaion-coach/applicaion-coach.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'vex-applicaion-coach',
+  templateUrl: './applicaion-coach.component.html',
+  styleUrls: ['./applicaion-coach.component.scss']
+})
+export class ApplicaionCoachComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
